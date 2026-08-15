bea043a7bb1e37c14100e8ba6665ff887a6a1cc4
Patrick Zimmer <taum@tuta.io>
2026-08-15 15:32:38 +0200

    chore: drop unused import in screenshot generator

--- test/screenshots/screenshots_test.dart
+++ test/screenshots/screenshots_test.dart
@@ -21,7 +21,6 @@ import 'package:catlog/src/screens/timeline_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Future<void> _loadRealFonts() async {
